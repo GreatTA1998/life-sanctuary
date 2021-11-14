055d6f26cd8cfff827a62da1cde798b6f50ff94c
Elton Lin <username@mit.edu>
2021-11-14 02:03:16 -0500

    Setup Vercel

---
+++ .vercel_build_output/static/_app/pages/MemoryDrawingsAndTimeline.svelte-6f9b17d6.js
@@ -0,0 +1,1 @@
+import{S as s,i as n,s as a}from"../chunks/vendor-257aeac0.js";class l extends s{constructor(e){super();n(this,e,null,null,a,{})}}export{l as default};
---
+++ .vercel_build_output/static/_app/pages/index.svelte-e9302ad9.js
@@ -0,0 +1,9 @@
+import{S as P,i as T,s as q,k as f,e as g,t as y,j as $,P as A,d as l,n as h,c as j,a as x,g as k,m as O,b as S,f as b,I as i,o as w,J as D,x as E,u as M,v as C}from"../chunks/vendor-257aeac0.js";import F from"./JordanPeterson.svelte-5b71c0a7.js";import H from"./MusicAndVisions.svelte-16840eae.js";/* empty css                                                    */function I(J){let o,t,r,m,d,s,u,n,_,c;return s=new F({}),n=new H({}),{c(){o=f(),t=g("section"),r=g("h1"),m=y("Life Sanctuary"),d=f(),$(s.$$.fragment),u=f(),$(n.$$.fragment),_=y(`
+
+	TODO: 
+		2. Memory drawings and history
+	  3. Fun places and things to do`),this.h()},l(e){A('[data-svelte="svelte-1anpopb"]',document.head).forEach(l),o=h(e),t=j(e,"SECTION",{class:!0});var a=x(t);r=j(a,"H1",{class:!0});var v=x(r);m=k(v,"Life Sanctuary"),v.forEach(l),d=h(a),O(s.$$.fragment,a),u=h(a),O(n.$$.fragment,a),_=k(a,`
+
+	TODO: 
+		2. Memory drawings and history
+	  3. Fun places and things to do`),a.forEach(l),this.h()},h(){document.title="Home",S(r,"class","svelte-mjk9ig"),S(t,"class","svelte-mjk9ig")},m(e,p){b(e,o,p),b(e,t,p),i(t,r),i(r,m),i(t,d),w(s,t,null),i(t,u),w(n,t,null),i(t,_),c=!0},p:D,i(e){c||(E(s.$$.fragment,e),E(n.$$.fragment,e),c=!0)},o(e){M(s.$$.fragment,e),M(n.$$.fragment,e),c=!1},d(e){e&&l(o),e&&l(t),C(s),C(n)}}}const z=!0;class B extends P{constructor(o){super();T(this,o,null,I,q,{})}}export{B as default,z as prerender};
